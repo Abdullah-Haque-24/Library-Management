4202124f55fffc2f649f211a77f36d2deeb606b3
Abdullah-Haque-24 <abdullahhaque24@gmail.com>
2024-08-27 22:09:29 +0600

    changes made

--- library_management/urls.py
+++ library_management/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from .views import home
 from django.conf.urls.static import static
 from library_management import settings
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +12,8 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('core/', include('core.urls')),
     path('category/<slug:book_slug>/', home, name = 'category_wise_books'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
